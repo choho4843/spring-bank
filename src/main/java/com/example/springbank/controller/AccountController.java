6a32a7705aec09a4c9cfa73eec7f79dce5ada4be
choho4843 <choho4843@gmail.com>
2022-11-22 11:42:28 +0900

    AccountInfo add

--- src/main/java/com/example/springbank/controller/AccountController.java
+++ src/main/java/com/example/springbank/controller/AccountController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,5 +27,25 @@ public class AccountController {
             res= new ResponseEntity<String>("error", HttpStatus.BAD_GATEWAY);
         }
         return res;
+    }
+    @PostMapping("/accinfo")
+    public  ResponseEntity<Account> accinfo(@RequestParam("id") String id){
+        ResponseEntity<Account> res = null;
+        System.out.println("id:" +id);
+        try{
+            Account acc = accountService.accountInfo(id);
+            res = new ResponseEntity<Account>(acc, HttpStatus.OK);
+        } catch (Exception e){
+            e.printStackTrace();
+            res= new ResponseEntity<Account>(new Account(), HttpStatus.BAD_REQUEST);
+        }
+        return  res;
+    }
+    @PostMapping("/doubleid")
+    public  ResponseEntity<String> doubldId(@RequestParam("id") String id){
+        ResponseEntity<String> res = null;
+        try{
+
+        }
     }
 }
--- src/main/java/com/example/springbank/service/AccountService.java
+++ src/main/java/com/example/springbank/service/AccountService.java
@@ -5,6 +5,8 @@ import com.example.springbank.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class AccountService {
     @Autowired
@@ -13,5 +15,15 @@ public class AccountService {
     public  void makeAccount(Account acc) throws  Exception{
         acc.setBalance(0);
         accountRepository.save(acc);
+    }
+    public  Account accountInfo(String id) throws Exception {
+        Optional<Account> oacc = accountRepository.findById(id);
+        if(!oacc.isPresent()) throw new Exception("계좌번호 오류");
+        return oacc.get();
+    }
+    public String checkDoubleId(String id) throws Exception{
+        Optional<Account> oacc = accountRepository.findById(id);
+        if(oacc.isPresent()) return "true";
+        return  "false";
     }
 }
